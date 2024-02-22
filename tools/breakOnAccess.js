920c2e0ed9ce5ca640b669052010cd8fc833a970
Gilberto Stankiewicz <874134+kiewic@users.noreply.github.com>
2024-02-22 08:27:02 -0800

    Adding tools to break on array access

--- tools/breakOnAccess.js
+++ tools/breakOnAccess.js
@@ -1,14 +1,14 @@
-window.breakOn = function(obj, propertyKey) {
+window.break1 = function (obj, propertyKey) {
     let original = Object.getOwnPropertyDescriptor(obj, propertyKey);
     let localValue;
     Object.defineProperty(obj, propertyKey, {
         enumerable: original ? original.enumerable : true,
         configurable: original ? original.configurable : true,
-        get: function() {
+        get: function () {
             debugger;
             return original ? original.value : localValue;
         },
-        set: function(newValue) {
+        set: function (newValue) {
             if (original) {
                 original.value = newValue;
             }
@@ -18,3 +18,26 @@ window.breakOn = function(obj, propertyKey) {
         },
     });
 }
+
+
+const handler = {
+    get(target, prop, receiver) {
+        debugger;
+        // Return the accessed property
+        return Reflect.get(...arguments);
+    }
+};
+newHierarchy = new Proxy(newHierarchy, handler);
+
+
+const handler = {
+    set(target, prop, value, receiver) {
+        // Check if the property being set is a numeric index
+        if (!isNaN(prop)) {
+            debugger;
+        }
+        // Set the property value
+        return Reflect.set(...arguments);
+    }
+};
+newHierarchy.leafNodes  = new Proxy(newHierarchy.leafNodes, handler);
